fba79635df2f0017dacc7679c560bb1234001216
Mikita Makeichyk <mikitamakeichyk@gmail.com>
2025-02-02 00:28:50 +0400

    feat: add Main component to the App

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import CardList from './CardList';
+
+class Main extends Component {
+  render() {
+    return (
+      <>
+        <CardList />
+      </>
+    );
+  }
+}
+
+export default Main;
